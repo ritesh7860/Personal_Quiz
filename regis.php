013807ba377420c5a107d644aec932b3deab1dd0
Deepali <deepalisingh3233@gmail.com>
2025-09-08 16:22:20 +0530

    basic chnages

--- regis.php
+++ regis.php
@@ -6,9 +6,6 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>WIN OR BOOZE</title>
-   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.js"></script>
-   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
    <script type="text/javascript">
       function f() {
          var result = true;
@@ -39,96 +36,21 @@
 
       }
    </script>
+   <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
    <style>
-      body {
-
-         background-image: black;
-         background-image: url('background.avif');
-         background-size: cover;
-         background-attachment: fixed;
-      }
-
-      hr {
-         border-top: 3px solid rgba(0, 0, 0, 1);
-      }
-
-      nav {
-         padding: 0px 20px 0px 20px;
-         font-size: 18px;
-         font-family: cursive;
-         background-color: #ad5fa7ff !important;
-      }
-
-      .hading {
-         align-items: center;
-         color: #b0bed3;
-         font-size: 40px;
-         font-weight: 200;
-         font-family: cursive;
-      }
-
-      .form {
-         color: #ffffffff;
-         font-size: 16px;
-         position: absolute;
-         right: 30%;
-         bottom: 20%;
-         width: 35%;
-         height: 38%;
-         top: 35%;
-         border: 0px solid gray;
-
-      }
-
-      div form input[type=submit] {
-         border: 2px solid rgb(71, 80, 82);
-         background-color: #ad5fa7;
-         color: #ffffffff;
-         font-size: 14px;
-         border-radius: 10px;
-         font-weight: 700;
-         padding: 3px 50px;
-
-      }
-
-      div form input[type=text],
-      div form input[type=email],
-      div form input[type=password] {
-         background-color: white;
-         color: black;
-         border-radius: 10px;
-      }
-
-
-
-
-      div form input[type=text]:focus,
-      div form input[type=email]:focus,
-      div form input[type=password]:focus {
-         border: 3px solid rgb(163, 170, 172);
-         border-radius: 10px;
-      }
-   </style>
+    .main{
+      background-image: url("https://t4.ftcdn.net/jpg/04/39/13/37/360_F_439133751_qALNX4ZKuphjpCLPGtASIvoUxwUNLxk2.jpg");
+      background-repeat: no-repeat;
+      /* background-position: center center; */
+      background-size: cover;
+      background-attachment: fixed;
+      
+    }
+  </style>
 </head>
 
 <body>
-
-   <div class="container-fluid">
-      <nav class="navbar navbar-inverse navbar-fixed-top">
-         <div class="container-fluid">
-            <ul class="nav navbar-nav">
-               <li><a href="login2.php">Home</a></li>
-               <li><a href="regis.php">Register</a></li>
-               <li><a href="#">About Us</a></li>
-               <li><a href="#">FAQ</a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-               <li><a href="#">Logout</a></li>
-            </ul>
-         </div>
-      </nav>
-   </div>
-   <div class="form">
+   <div class="form main h-screen w-screen ">
       <form align="center" method="post" name="frm" onsubmit="return f()">
          <div class="hading">
             <h3 style="color: #ffffffff; font-weight:600; font-size:35px"> Create Your Account</h3>
@@ -136,7 +58,7 @@
             </hr>
          </div>
 
-         <div style="display: flex; flex-direction:column; gap: 20px; padding-top: 10px; padding-bottom: 40px; width: 80%; margin: auto;">
+         <div style="display: flex; flex-direction:column; gap: 20px; padding-bottom: 40px; width: 80%; margin: auto;">
             <div style="display: flex; justify-content:space-between">
                <label>Name :</label>
                <input type="text" placeholder=" Enter Your Name" name="name" id="name" />
